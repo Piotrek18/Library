33d2a00a281bc34bc2683b6f2f36971faad19f4b
Piotrek18 <piotrbielinsk@gmail.com>
2024-03-24 13:59:43 +0100

    Create Book and addBookToLibrary functions

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-function Book(title, author, pages, read){
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
-
-    this.info = function(){
-        const readStatus = this.read ? "read" : "not read yet";
-        return `${this.title} by ${this.author}, ${this.pages}, ${readStatus}`;
-    };
 };
+
+function addBookToLibrary(){
+    const newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);
+}
 
 const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295', false);
 
